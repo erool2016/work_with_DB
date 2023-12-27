26ed8dd9425580df64865a9a74309046fec1dc6a
erool2016 <erool2016@yandex.ru>
2023-12-27 16:51:50 +0300

    OrderItem

--- main1/models.py
+++ main1/models.py
@@ -29,3 +29,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Order(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    addres = models.CharField(max_length=50,)
+    notes = models.TextField(blank=True)
+    product = models.ManyToManyField(Product, through='OrderItem')
+    def __str__(self):
+        return f'Заказ N: {self.id}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
